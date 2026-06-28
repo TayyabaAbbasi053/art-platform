b97f9f760d4e9b6189eace4c6014e0620d017f4c
YourGitHubUsername <tabbasi.bscs24seecs@seecs.edu.pk>
2026-06-29 01:33:07 +0500

    fixed all minor ui issue on public pages

--- order-confirmation.php
+++ order-confirmation.php
@@ -353,6 +353,7 @@ img{max-width:100%;display:block;}
     <div class="nlinks">
       <a href="artworks.php">Explore Art</a>
       <a href="artists.php">Artists</a>
+      <a href="blog.php">Blog</a>
       <a href="commission.php">Custom Artwork</a>
       <a href="sell.php">Sell Your Art</a>
       <a href="about.php">About Us</a>
@@ -363,7 +364,6 @@ img{max-width:100%;display:block;}
       <input type="text" placeholder="Search...">
     </div>
     <div class="nend">
-      <span style="font-size:12.5px;color:var(--bg);">Welcome, <?= htmlspecialchars($_SESSION['name'] ?? 'Buyer') ?></span>
       <a href="logout.php" class="btn-ghost">Logout</a>
 
       <button class="ham-btn" aria-label="Open menu">
@@ -678,6 +678,7 @@ img{max-width:100%;display:block;}
   <div class="drawer-links">
     <a href="artworks.php">Explore Art</a>
     <a href="artists.php">Artists</a>
+    <a href="blog.php">Blog</a>
     <a href="commission.php">Custom Artwork</a>
     <a href="sell.php">Sell Your Art</a>
     <a href="about.php">About Us</a>
